24acf2d7b1f89585e90d982f21b8e1330f7a9faa
Nabil Ridhwanshah <nabilrdhwn@gmail.com>
2018-11-03 06:12:45 +0800

    Removed Pull To Refresh (Bus Timings page)

--- app/components/BusTiming.vue
+++ app/components/BusTiming.vue
@@ -3,45 +3,48 @@
         <ActionBar :title="busstopname + '(' +busstopnumber + ')'" class="action-bar" />
         <ActionBar v-if="!busstopname" :title="'(' +busstopnumber + ')'" class="action-bar" />
 
-        <PullToRefresh @refresh="refreshList">
 
-                <StackLayout>
-                    <ListView class="list-group" for="bus in busdata.services" v-if="app.mode == 'view_bus' && loaded.busdata">
-                        <v-template>
+        <!-- <PullToRefresh @refresh="refreshList"> -->
+            <StackLayout>
 
-                            <StackLayout class="list-group-item">
+                <Button @tap="refreshList">Refresh</Button>
+                
+                <ListView class="list-group" for="bus in busdata.services" v-if="app.mode == 'view_bus' && loaded.busdata">
+                    <v-template>
 
-                                <!-- DISPLAY BUS NUMBER -->
-                                <Label :text="bus.no" class="list-group-item-heading own-header" />
+                        <StackLayout class="list-group-item">
 
-                                <!-- BUS TIMING -->
-                                <!-- DISPLAY TRUE TIME IF BUS ARRIVAL TIME IS ABOVE 1 MIN -->
-                                <Label v-if="Math.floor(bus.next.duration_ms / 60000) > 1" class="list-group-item-heading">🕗 {{Math.floor(bus.next.duration_ms / 60000) + ' Mins' }}</Label>
-                                <!-- SHOWS ARRIVING IF BUS ARRIVAL TIME IS UNDER 1 MIN -->
-                                <Label v-else text="🕗 Arriving" class="list-group-item-heading" />
+                            <!-- DISPLAY BUS NUMBER -->
+                            <Label :text="bus.no" class="list-group-item-heading own-header" />
 
-                                <!-- BUS FEATURE -->
-                                <Label v-if="bus.next.feature == 'WAB'" textWrap="true" class="list-group-item-heading">☑️ Wheelchair </Label>
-                                <Label v-else textWrap="true" class="list-group-item-heading">❎ Wheelchair </Label>
+                            <!-- BUS TIMING -->
+                            <!-- DISPLAY TRUE TIME IF BUS ARRIVAL TIME IS ABOVE 1 MIN -->
+                            <Label v-if="Math.floor(bus.next.duration_ms / 60000) > 1" class="list-group-item-heading">🕗 {{Math.floor(bus.next.duration_ms / 60000) + ' Mins' }}</Label>
+                            <!-- SHOWS ARRIVING IF BUS ARRIVAL TIME IS UNDER 1 MIN -->
+                            <Label v-else text="🕗 Arriving" class="list-group-item-heading" />
 
-                                <!-- BUS TYPE (SINGLE DECK OR DOUBLE DECK) -->
-                                <!-- SHOW SINGLE DECK IF BUS.NEXT.TYPE IS SD -->
-                                <Label v-if="bus.next.type == 'SD'" textWrap="true" class="list-group-item-heading">🚌 Single Deck</Label>
-                                <!-- SHOW DOUBLE DECK IF BUS.NEXT.TYPE IS DD -->
-                                <Label v-if="bus.next.type == 'DD'" textWrap="true" class="list-group-item-heading">🚌 Double Deck</Label>
+                            <!-- BUS FEATURE -->
+                            <Label v-if="bus.next.feature == 'WAB'" textWrap="true" class="list-group-item-heading">☑️ Wheelchair </Label>
+                            <Label v-else textWrap="true" class="list-group-item-heading">❎ Wheelchair </Label>
 
-
-                            </StackLayout>
-
-                        </v-template>
-                    </ListView>
-
-                    <!-- SHOWS LOADING WHILE LOADING BUS STOP DATA -->
-                    <!-- <Label v-else>Loading...</Label> -->
-                </StackLayout>
+                            <!-- BUS TYPE (SINGLE DECK OR DOUBLE DECK) -->
+                            <!-- SHOW SINGLE DECK IF BUS.NEXT.TYPE IS SD -->
+                            <Label v-if="bus.next.type == 'SD'" textWrap="true" class="list-group-item-heading">🚌 Single Deck</Label>
+                            <!-- SHOW DOUBLE DECK IF BUS.NEXT.TYPE IS DD -->
+                            <Label v-if="bus.next.type == 'DD'" textWrap="true" class="list-group-item-heading">🚌 Double Deck</Label>
 
 
-        </PullToRefresh>
+                        </StackLayout>
+
+                    </v-template>
+                </ListView>
+
+                <!-- SHOWS LOADING WHILE LOADING BUS STOP DATA -->
+                <Label v-else>Loading...</Label>
+            </StackLayout>
+        <!-- </PullToRefresh> -->
+
+
 
     </Page>
 </template>
@@ -130,7 +133,7 @@
     }
 
     Button {
-        margin: 5 0;
+        margin: 10 0;
         color: white;
         background-color: #e24747;
         border-radius: 1000;
